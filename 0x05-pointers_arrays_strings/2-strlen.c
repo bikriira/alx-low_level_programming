cce796e13059f599ec15e5468ca5ab6d62dfa25e
bikriira <iradufashabikri@gmail.com>
2024-01-10 01:56:08 -0800

    Feat: a function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * _strlen - Counts the number of character in the string
+ * @s: Pointer to the string to be worked on
+ *
+ * Return: The number of string chars
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
