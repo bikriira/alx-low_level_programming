5ad8c95d114e76958d94981ea8d3054d073b071a
bikriira <iradufashabikri@gmail.com>
2024-01-16 01:41:48 -0800

    [Fix] stopping iteration before time

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	while (curr->next != NULL)
+	while (curr != NULL)
 	{
 		if (counter + 1 == idx)
 		{
