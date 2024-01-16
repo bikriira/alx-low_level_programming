291faa296ff5ea2b8e5580f4dd97dc58ab40875a
bikriira <iradufashabikri@gmail.com>
2024-01-16 00:26:28 -0800

    [Feat] Implement a function that adds a new node at the end of a dlistint_t list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,50 +1,36 @@
 #include "lists.h"
 
-/**
- * add_dnodeint_end - function that adds node at the end of a list.
- * @head: pointer to pointer to head of list.
- * @n: data to be added.
- *
- * Return: address of the new element or NULL if it failed.
- */
 
+/**
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list
+ * @head: the address of the list's head
+ * @n: The value to be stored inside the new node's n
+ *
+ * Return: The address of the new element, or NULL if it failed
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node, *last;
+	dlistint_t *curr = *head;
+	dlistint_t *my_node = malloc(sizeof(dlistint_t));
 
-	/* allocate memory for new node */
-	new_node = malloc(sizeof(dlistint_t));
-
-	/* if it fails return NULL */
-	if (new_node == NULL)
+	if (my_node == NULL)
 		return (NULL);
 
-	/* assign data to new node */
-	new_node->n = n;
+	my_node->n = n;
+	my_node->next = NULL;
 
-	/**
-	 * the new node is going to be the last node so make next,
-	 * of it as NULL
-	 */
-	new_node->next = NULL;
-
-	last = *head;
-
-	/* if there is no head/list make new node as head node */
-	if (*head == NULL)
+	if (curr == NULL)
 	{
-		new_node->next = *head;
-		*head = new_node;
+		my_node->prev = NULL;
+		*head = my_node;
+		return (my_node);
 	}
-	else
-	{
-		/* traverse till last node */
-		while (last->next)
-			last = last->next;
-		/* change the next of last node */
-		last->next = new_node;
-	}
-	new_node->prev = last;
 
-	return (new_node);
+	while (curr->next != NULL)
+		curr = curr->next;
+
+	curr->next = my_node;
+	my_node->prev = curr;
+
+	return (my_node);
 }
