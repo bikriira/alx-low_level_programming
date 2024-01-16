e27f68978baf1dc9c8da3906f05ec98523d24d3d
bikriira <iradufashabikri@gmail.com>
2024-01-16 01:54:06 -0800

    [Fix] curr->prev should be updated if inserting at index 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,6 @@
 #include  "lists.h"
 
 
-
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position
  * @h: The address of list's head
@@ -23,6 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->prev = NULL;
 		new_node->next = curr;
+		curr->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
