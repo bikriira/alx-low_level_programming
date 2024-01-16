5ef1d7910e0ebf9a314dd9b72c16cb2218bc2cee
bikriira <iradufashabikri@gmail.com>
2024-01-16 02:03:37 -0800

    [Fix] must upadate curr->prev whenever required

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,6 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			curr->next = new_node;
 			if (curr->next != NULL)
 				curr->next->prev = new_node;
+
 			return (new_node);
 		}
 		counter++;
