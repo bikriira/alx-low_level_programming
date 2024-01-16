52fb9e29ae8a1f431cb5bca72ab21c237937a745
bikriira <iradufashabikri@gmail.com>
2024-01-16 02:56:11 -0800

    [Feat] Implement a function that deletes the node at index index of a dlistint_t linked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,50 +1,50 @@
-#include "lists.h"
+#include  "lists.h"
+
 
 /**
- * delete_dnodeint_at_index - function that deletes the node at index of a,
- * linked list.
- * @head: pointer to pointer to the h of linked list.
- * @index: ndex of the node that should be deleted. Index starts at 0.
+ * delete_dnodeint_at_index - Deletes node at a given position
+ * @head: The address of list's head
+ * @index: Index of the node to delete
  *
- * Return: if it succeeded, -1 if it failed
+ * Return: The address of the new node, or NULL if it failed
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *head1;
-	dlistint_t *head2;
-	unsigned int count;
+	unsigned int counter = 0;
+	dlistint_t *curr = *head, *node_to_delete;
 
-	head1 = *head;
+	if (curr == NULL)
+		return (-1);
 
-	if (head1 != NULL)
-		while (head1->prev != NULL)
-			head1 = head1->prev;
-	count = 0;
-
-	while (head1 != NULL)
+	if (index == 0)
 	{
-		if (count == index)
-		{
-			if (count == 0)
-			{
-				*head = head1->next;
-				if (*head != NULL)
-					(*head)->prev = NULL;
-			}
-			else
-			{
-				head2->next = head1->next;
+		node_to_delete = curr;
 
-				if (head1->next != NULL)
-					head1->next->prev = head2;
-			}
-			free(head1);
+		if (curr->next != NULL)
+			curr->next->prev = NULL;
+
+		free(node_to_delete);
+		*head = curr->next;
+
+		return (1);
+	}
+
+	while (curr->next != NULL)
+	{
+		if (counter + 1 == index)
+		{
+			node_to_delete = curr->next;
+			curr->next = node_to_delete->next;
+
+			if (node_to_delete->next != NULL)
+				node_to_delete->next->prev = curr;
+
+			free(node_to_delete);
 			return (1);
 		}
-		head2 = head1;
-		head1 = head1->next;
-		count++;
+		counter++;
+		curr = curr->next;
 	}
+
 	return (-1);
 }
