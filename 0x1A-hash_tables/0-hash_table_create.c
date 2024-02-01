343b34238a2d3a27d228e28ae4996138ba6cc287
bikriira <iradufashabikri@gmail.com>
2024-02-01 02:32:49 -0800

    [Fix] Betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = malloc(sizeof(hash_table_t) * size);
 
 	if (table == NULL)
-		return NULL;
+		return (NULL);
 	else
-		return table;
+		return (table);
 }
