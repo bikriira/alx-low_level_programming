26b9c0ed050782b8c9a6513c92f125edcf811b0b
bikriira <iradufashabikri@gmail.com>
2024-02-01 09:20:16 -0800

    [Try] i don't need to check if the aray[idx] is null🤷‍♀️

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,9 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	elem_index = key_index((const unsigned char *)key, ht->size);
 
-	if (ht == NULL || ht->array[elem_index] == NULL || strlen(key) == 0)
-		return (NULL);
-	if (elem_index >= ht->size)
+	if (ht == NULL || strlen(key) == 0)
 		return (NULL);
 
 	current = ht->array[elem_index];
