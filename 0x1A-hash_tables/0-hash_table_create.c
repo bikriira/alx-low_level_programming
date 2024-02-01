77fc3b3664424878aed4a04eda27cb1041c68b16
bikriira <iradufashabikri@gmail.com>
2024-02-01 02:52:18 -0800

    [Fix] allocate memorry for the array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t) * size);
+	unsigned long int i = 0;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
-	else
-		return (table);
+
+	table->size = size;
+
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+		return (NULL);
+
+	while (i > size)
+	{
+		table->array[i] = NULL;
+		i++;
+	}
+
+	return (table);
 }
