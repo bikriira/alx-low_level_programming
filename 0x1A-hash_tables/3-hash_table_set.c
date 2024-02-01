2a9b4cf998a54311c80b1e86f05f1a04049113b2
bikriira <iradufashabikri@gmail.com>
2024-02-01 07:46:54 -0800

    [Try] check if value is null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int element_index;
 	hash_node_t *element = malloc(sizeof(hash_node_t));
 
-	if (element == NULL || strlen(key) == 0 || key == NULL || ht == NULL)
+	if (element == NULL || strlen(key) == 0 || ht == NULL || value == NULL)
 		return (0);
 
 	element->key = strdup(key);
