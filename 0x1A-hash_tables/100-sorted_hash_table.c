6a0b9101f86bf59613ff9fa313a8d24ab5c1f421
bikriira <iradufashabikri@gmail.com>
2024-02-03 00:28:31 -0800

    [Fix] requirement, return 1 not 10😁

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -67,13 +67,10 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	{
 		free(ht->array[index]->value);
 		ht->array[index]->value = strdup(value);
-		if ((ht->array[index]->value) == NULL)
-		{
-			free(ht->array[index]->key);
-			free(ht->array[index]->value);
-			free(element);
-		}
-		return (10);
+		free(element->key);
+		free(element->value);
+		free(element);
+		return (1);
 	}
 	else
 	{
